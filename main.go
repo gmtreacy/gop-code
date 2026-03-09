71c316a243a8f73dd0809962b69b46f5288a7fb3
George Treacy <george_treacy@technologyonecorp.com>
2026-03-09 15:01:39 +1000

    boomer

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 func handleHealthz(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK zoomer"))
+	w.Write([]byte("OK boomer"))
 }
 
 func handleReadyz(w http.ResponseWriter, r *http.Request) {
